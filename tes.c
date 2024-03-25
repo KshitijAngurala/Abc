eea5e0bca5040ec09ea7b74a95e1f0883942232b
KshitijAngurala <164717320+KshitijAngurala@users.noreply.github.com>
2024-03-25 21:04:03 +0530

    Add files via upload

---
+++ tes.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(void){
+    int x = 20;
+    int y = 34;
+
+    printf("x is: %d\n",x);
+    printf("y is: %d\n",y);
+
+
+    // swapping.......
+
+    printf("x is now: %d\n",x);
+    printf("y is now: %d\n",y);
+}
